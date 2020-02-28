b79b7d14fab2ef73dc291a876d132c293568e8b2
VitaliiChor <53510366+VitaliiChor@users.noreply.github.com>
2020-02-28 16:11:59 +0200

    Payment form 2.2

--- payment_form/js/script.js
+++ payment_form/js/script.js
@@ -1,11 +1,16 @@
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
-
 $('#success-message').hide();
+//Close button
+$('.close').click(function(){
+  $('.close').hide();
+  $('#pay-invoice').hide();
+})
 
+
+//Payment button
 $("#payment-button").click(function (e) {
-
 
   let form = $(this).parents('form');
 
@@ -48,11 +53,35 @@ $("#payment-button").click(function (e) {
     
     $('#success-message').show();
     $('#pay-invoice').hide();
-    
+    $('.close').hide();
   }
   form.addClass('was-validated');
 
 })
+
+//Timer
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.text(minutes + ":" + seconds);
+
+      if (--timer < 0) {
+          timer = duration;
+      }
+  }, 1000);
+}
+
+jQuery(function ($) {
+  var fiveMinutes = 60 * 12,
+      display = $('.time');
+  startTimer(fiveMinutes, display);
+});
 
 
 
